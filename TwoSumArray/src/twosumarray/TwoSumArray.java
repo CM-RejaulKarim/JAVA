d1071280f84902c1c821fd378e1f70401496f0ed
C.M. Rejaul Karim <cmrejaulkarim@gmail.com>
2025-06-03 12:34:01 +0600

    error

---
+++ TwoSumArray/src/twosumarray/TwoSumArray.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package twosumarray;
+
+/**
+ *
+ * @author user
+ */
+public class TwoSumArray {
+
+    /**
+     * @param args the command line arguments
+     */
+//    public static void main(String[] args) {
+//        int[] nums ={2,7,11,15};
+//        int target = 9;
+//        
+//      int[] calculate =twosumm 
+//        
+//        
+//    }
+//    public int[] twoSumMethod(int[] nums){
+//       int target=0;
+//    int[] result =new int[2];
+//    for (int i = 0; i < nums.length; i++) {
+//            for (int j = 1; j < nums.length-1; j++) {
+//                if (nums[i]+nums[j]==target) {
+//                    
+//                    result = {nums[i],nums[j]};
+//                    
+//                    
+//                    return result;
+//                            
+//                }
+//                
+//            }
+//        }
+//    
+//    
+//    }
+    
+}
